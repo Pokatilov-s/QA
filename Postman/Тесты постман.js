94ce2b9f6cde4e36471b6b21a5d924bd340fa20b
Семён <120897217+Pokatilov-s@users.noreply.github.com>
2023-12-06 19:08:50 +0700

    Update Тесты постман.js

--- Postman/Тесты постман.js
+++ Postman/Тесты постман.js
@@ -1,3 +1,14 @@
+//Описание 
 var id_task = pm.response.json().tasks[0].id;
 
 pm.collectionVariables.set("task_id", id_task);
+
+//Описание 
+var jsonData = pm.response.json()
+malahov = jsonData.suggestions[2].data.surname
+
+console.log(malahov) 
+
+pm.test("Ищем Малахова", function () {
+    pm.expect(malahov).to.eql('Малахов');
+});
